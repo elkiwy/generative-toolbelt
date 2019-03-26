55e44dc3765586c38981118c5ca02facd7a4c257
Stefano Bertoli <elkiwydev@gmail.com>
2019-03-26 20:46:47 +0100

    Added blank project

---
+++ .#project.clj
@@ -0,0 +1,1 @@
+stefanobertoli@KiwyBookPro.local.2488
---
+++ src/generative_toolbelt/core.clj
@@ -0,0 +1,11 @@
+(ns generative-toolbelt.core
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+
+
+(defn testone
+  "Prints a testone stirng"
+  []
+  (printnl "testone!"))
+

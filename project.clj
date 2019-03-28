a237a94cda8cd19bd59f6069fdd20aba5958f347
Stefano Bertoli <elkiwydev@gmail.com>
2019-03-28 19:09:54 +0100

    Updated project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject generative-toolbelt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject elkiwy/generative-toolbelt "0.1.0-SNAPSHOT"
+  :description "A handy set of function to ease out the creative process of drawing with Quil."
+  :url "https://github.com/elkiwy/generative-toolbelt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]

9c3cf9e81910f73160220112b48a45ba0409d4c2
Stefano Bertoli <elkiwydev@gmail.com>
2019-03-31 21:10:35 +0200

    Added Codox plugin to project.clj for the auto-documentation.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/elkiwy/generative-toolbelt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins       [[lein-codox "0.10.6"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [quil "2.8.0"]])

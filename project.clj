6d0ae07f41275868aa249113579d6568f3c8eeeb
Stefano Bertoli <elkiwydev@gmail.com>
2019-04-02 21:59:54 +0200

    Updated license in project.clj and moved to version 0.1.1

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject elkiwy/generative-toolbelt "0.1.0-SNAPSHOT"
+(defproject elkiwy/generative-toolbelt "0.1.1-SNAPSHOT"
   :description "A handy set of function to ease out the creative process of drawing with Quil."
   :url "https://github.com/elkiwy/generative-toolbelt"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU General Public License v3.0"
+            :url "https://www.gnu.org/licenses/gpl-3.0.en.html"}
   :plugins       [[lein-codox "0.10.6"]]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [quil "2.8.0"]])

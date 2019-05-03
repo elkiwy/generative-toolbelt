5bbe6d30a6f42a4de13dbb26495d54ec98c80665
Stefano Bertoli <stefano.bertoli@sbsmobile.it>
2019-05-03 08:51:37 +0200

    Started some work on the color module

--- src/generative_toolbelt/color.clj
+++ src/generative_toolbelt/color.clj
@@ -20,3 +20,25 @@
 
 
 
+
+
+(defn save-stroke-settings 
+    "Saves the current stroke settings into a map."
+    ([]
+        {:stroke (quil/current-stroke) :fill (quil/current-fill)})
+
+    ([strokeWeight]
+        {:stroke (quil/current-stroke) :stroke-weight strokeWeight :fill (quil/current-fill)}))
+        
+
+(defn set-stroke-settings
+    "Sets all the previously saved stroke settings"
+    [{:keys [stroke stroke-weight fill]}]
+        (quil/stroke stroke)
+        (quil/fill fill)
+        (when (not (nil? stroke-weight))
+            (quil/stroke-weight stroke-weight)))
+
+
+
+
